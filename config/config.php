fcbb0c108a1a2979133f3c567a21348dfd6c25fd
Gene <genavarov@gmail.com>
2023-01-06 14:07:26 +0300

    ladablock

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+/******************************************************************************
+	Original By https://github.com/stolendata/rpc-ace 
+	Tuned by http://www.blockpioneers.info - Bitcointalk : BanzaiBTC
+******************************************************************************/
+
+define( 'RPCHOST', '127.0.0.1' );
+define( 'RPCPORT', 8832 );
+define( 'RPCUSER', 'ladacoinrpc' );
+define( 'RPCPASS', '7dFG9jAYThZVSies59' );
+
+define( 'COINNAME', 'LADAcoin' );
+define( 'COINPOS', false );
+
+define( 'RETURNJSON', false );
+define( 'DATEFORMAT', 'H:i:s Y-M-d' );
+define( 'BLOCKSPERLIST', 20 );
+
+// for the example explorer
+define( 'COINHOME', 'http://www.ladacoin.org' );
+define( 'REFRESHTIME', 3000 );
+?>
+
+
